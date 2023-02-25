a4ea439f1fd7aa53b5ac1e6829a681b8549fa6dd
Isnard <isnardgurgel@gmail.com>
2023-02-25 13:08:11 -0300

    new TEST updates

--- cloud_dict.py
+++ cloud_dict.py
@@ -129,5 +129,8 @@ else:
         search = st.text_input("Digite aqui a sua busca, depois clique em 'Procurar'")
         sub_2 = st.form_submit_button("Procurar")
         if sub_2:
-            st.write('Estamos consultando o termo:', search) 
+            st.write('Estamos consultando o termo:', search)
+            df = DataFrame(sh.worksheet('Edição - 1').get_all_records()) 
+            st.dataframe(df)
+            
 
