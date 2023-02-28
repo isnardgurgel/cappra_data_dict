f632689a8e6f3397ad5b2967bebed21345e69fe3
Isnard <isnardgurgel@gmail.com>
2023-02-28 15:49:28 -0300

    new TEST updates

--- cloud_dict.py
+++ cloud_dict.py
@@ -135,7 +135,6 @@ else:
             st.write('Estamos consultando o termo:', search)
             df_2= DataFrame(sh.worksheet('Edição - 1').get_all_records()) 
         
-        if df_2!="":
             st.write(df_2)
             
 
